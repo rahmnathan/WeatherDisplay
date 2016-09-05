471941b32c7bbe4821b5f4af41479552b24f54eb
rahmnathan <rahm.nathan@gmail.com>
2016-09-05 13:51:27 -0500

    GUI progress

--- GUI/src/main/java/GUI.java
+++ GUI/src/main/java/GUI.java
@@ -1,13 +1,14 @@
 import javax.swing.*;
 import java.awt.*;
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
-import java.util.Calendar;
 
 public class GUI {
 
     private static JFrame frame = new JFrame("Weather Display");
     private static JPanel panel = new JPanel();
+    private static Color white = new Color(0xFFFFFF);
+    private static final int dateTimeHorizontal = 1370;
+    private static final int dateTimeVertical = 20;
 
     public static void startGUI(CurrentWeather currentWeather, byte[] backgroundImage){
 
@@ -28,29 +29,66 @@ public class GUI {
         panel.add(currentWeatherIcon);
 
         JLabel currentTemp = new JLabel("Current Temp");
-        currentTemp.setText(currentWeather.getTemp() + "F");
-        currentTemp.setLocation(90,120);
-        currentTemp.setSize(200,50);
-        currentTemp.setFont(new Font("Serif", Font.BOLD, 40));
-        currentTemp.setForeground(new Color(0xFFFFFF));
+        currentTemp.setText(currentWeather.getTemp() + "°F");
+        currentTemp.setLocation(230,20);
+        currentTemp.setSize(200,60);
+        currentTemp.setFont(new Font("Serif", Font.BOLD, 70));
+        currentTemp.setForeground(white);
         panel.add(currentTemp);
 
-        JLabel highTemp = new JLabel("High Temp");
-        highTemp.setText(currentWeather.getHighTemp() + "F");
-        highTemp.setLocation(5,110);
-        highTemp.setSize(100,30);
-        highTemp.setFont(new Font("Serif", Font.BOLD, 20));
-        highTemp.setForeground(new Color(0xFFFFFF));
-        panel.add(highTemp);
+        JLabel highLowTemps = new JLabel("High Temp");
+        highLowTemps.setText(currentWeather.getLowTemp().split("\\.")[0] + "°F/" + currentWeather.getHighTemp().split("\\.")[0] + "°F");
+        highLowTemps.setLocation(265,90);
+        highLowTemps.setSize(150,30);
+        highLowTemps.setFont(new Font("Serif", Font.BOLD, 20));
+        highLowTemps.setForeground(white);
+        panel.add(highLowTemps);
+
+        JLabel windSpeed = new JLabel("Wind Info");
+        windSpeed.setText(currentWeather.getWindSpeed().split("\\.")[0] + "Mph");
+        windSpeed.setLocation(250, 130);
+        windSpeed.setSize(150, 50);
+        windSpeed.setFont(new Font("Serif", Font.BOLD, 45));
+        windSpeed.setForeground(white);
+        panel.add(windSpeed);
+
+        JLabel windDirection = new JLabel("Wind Direction");
+        windDirection.setText(currentWeather.getWindDirection());
+        windDirection.setLocation(270, 190);
+        windDirection.setSize(200, 30);
+        windDirection.setFont(new Font("Serif", Font.BOLD, 30));
+        windDirection.setForeground(white);
+        panel.add(windDirection);
+
+        String[] dateTime = LocalDateTime.now().toString().split("T");
+        String[] timeStampArray = dateTime[1].split(":");
+        Integer hour = Integer.valueOf(timeStampArray[0]);
+        String hourString;
+        if(hour > 12){
+            hour -= 12;
+            hourString = "0" + hour;
+        } else{
+            hourString = String.valueOf(hour);
+        }
+        String timeStamp = hourString + ":" + timeStampArray[1];
+        String[] dateArray = dateTime[0].split("-");
+        String date = dateArray[1] + "-" + dateArray[2] + "-" + dateArray[0];
 
         JLabel currentTime = new JLabel("Current Time");
-        String timeStamp = LocalDateTime.now().toString();
-        currentTime.setSize(600, 200);
-        currentTime.setLocation(500, 50);
+        currentTime.setSize(500, 200);
+        currentTime.setLocation(dateTimeHorizontal, dateTimeVertical);
         currentTime.setText(timeStamp);
-        currentTime.setForeground(new Color(0xFFFFFF));
-        currentTime.setFont(new Font("Serif", Font.BOLD, 80));
+        currentTime.setForeground(white);
+        currentTime.setFont(new Font("Serif", Font.BOLD, 150));
         panel.add(currentTime);
+
+        JLabel currentDate = new JLabel("Current Date");
+        currentDate.setSize(500, 200);
+        currentDate.setLocation(dateTimeHorizontal + 85, dateTimeVertical + 110);
+        currentDate.setText(date);
+        currentDate.setFont(new Font("Serif", Font.BOLD, 50));
+        currentDate.setForeground(white);
+        panel.add(currentDate);
 
         backgroundPane.add(panel);
 
--- WeatherInfoProvider/src/main/java/CurrentWeatherProvider.java
+++ WeatherInfoProvider/src/main/java/CurrentWeatherProvider.java
@@ -78,19 +78,19 @@ public class CurrentWeatherProvider {
     private String getWindDirection(Integer degree){
 
         if(degree < 22.5 || degree > 337.5){
-            return "N";
+            return "North";
         } else if (degree < 67.5){
             return "NE";
         } else if (degree < 117.5){
-            return "E";
+            return "East";
         } else if (degree < 157.5){
             return "SE";
         } else if (degree < 202.5){
-            return "S";
+            return "South";
         } else if (degree < 247.5){
             return "SW";
         } else if (degree < 292.5){
-            return "W";
+            return "West";
         } else if (degree < 337.5){
             return "NW";
         } else{
