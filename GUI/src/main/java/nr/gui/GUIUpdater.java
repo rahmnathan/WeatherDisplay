4b52c0b76e1feae515d43db34f410d6536a195fa
rahmnathan <rahm.nathan@gmail.com>
2016-09-09 21:23:01 -0500

    Bug fix

--- GUI/src/main/java/nr/gui/GUIUpdater.java
+++ GUI/src/main/java/nr/gui/GUIUpdater.java
@@ -3,6 +3,8 @@ package nr.gui;
 import nr.currentweatherprovider.CurrentWeather;
 import org.apache.commons.lang3.text.WordUtils;
 
+import nr.gui.GUI.*;
+
 import java.awt.*;
 import java.time.LocalDateTime;
 
